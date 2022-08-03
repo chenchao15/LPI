772f9316043c85937eadff7ac237299ba7a8e777
chenchao <thss15_chenc@163.com>
2022-08-03 21:53:10 +0800

    add readme

--- run.sh
+++ run.sh
@@ -2,23 +2,23 @@
 
 classname=02691156
 python LPI.py \
-	--data_dir /data/cc/project/PUI/local_neuralpull_dataset/ \
+	--data_dir data/ \
         --pattern_num 100 \
         --batch_size 1 \
         --gauss_value 1 \
         --start_index 0 \
-	--out_dir /data/cc/neural_1018_1_0/ \
+	--out_dir output/ \
 	--class_idx $classname \
 	--train \
 	--dataset other \
 	--CUDA 6
 
 python LPI.py \
-        --data_dir /data/cc/project/PUI/local_neuralpull_dataset/ \
+        --data_dir data/ \
         --pattern_num 100 \
         --gauss_value 1 \
         --start_index 0 \
-        --out_dir /data/cc/neural_1018_1_0/ \
+        --out_dir output/ \
         --class_idx $classname \
         --dataset other \
         --CUDA 6
